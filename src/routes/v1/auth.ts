30ac3a8e10f759227974d2ad181e9963208efc98
Dozy <dotuan14747@gmail.com>
2025-06-16 14:50:03 +0300

    Added email and password validation for user registration

--- src/routes/v1/auth.ts
+++ src/routes/v1/auth.ts
@@ -1,9 +1,47 @@
 import { Router } from "express";
+import { body } from "express-validator";
 
 import register from "@/controllers/v1/auth/register";
 
+import validationError from "@/middlewares/validationError";
+
+import User from "@/models/user";
+
 const router = Router();
 
-router.post("/register", register);
+router.post(
+  "/register",
+  body("email")
+    .trim()
+    .notEmpty()
+    .withMessage("Email is required")
+    .isLength({ max: 50 })
+    .withMessage("Email cannot exceed 50 characters")
+    .isEmail()
+    .withMessage("Invalid email address")
+    .custom(async (value) => {
+      const userExists = await User.exists({ email: value });
+      if (userExists) {
+        throw new Error("User already exists");
+      }
+    }),
+  body("password")
+    .notEmpty()
+    .withMessage("Password is required")
+    .isLength({ min: 8 })
+    .withMessage("Password must be at least 8 characters long")
+    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).+$/)
+    .withMessage(
+      "Password must contain at least 1 lowercase letter, 1 uppercase letter, 1 number, and 1 symbol"
+    ),
+  body("role")
+    .optional()
+    .isString()
+    .withMessage("Role must be a string")
+    .isIn(["admin", "user"])
+    .withMessage("Role must be either 'admin' or 'user'"),
+  validationError,
+  register
+);
 
 export default router;
