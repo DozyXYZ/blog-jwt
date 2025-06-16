19f6de232d213d26589c7bf74b092e5d6a75a100
Dozy <dotuan14747@gmail.com>
2025-06-16 09:19:14 +0300

    Initialized

---
+++ src/server.ts
@@ -0,0 +1,52 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import helmet from "helmet";
+
+import config from "@/config";
+import limiter from "@/lib/express_rate_limit";
+
+import type { CorsOptions } from "cors";
+
+const app = express();
+
+const corsOptions: CorsOptions = {
+  origin(origin, callback) {
+    if (!origin || config.NODE_ENV === "development") {
+      callback(null, true);
+    } else {
+      callback(
+        new Error(`CORS error: ${origin} is not allowed by CORS`),
+        false
+      );
+      console.error(`CORS error: ${origin} is not allowed by CORS`);
+    }
+  },
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(compression({ threshold: 1024 }));
+app.use(helmet());
+app.use(limiter);
+
+(async () => {
+  try {
+    app.get("/", (req, res) => {
+      res.json({ message: "Welcome to the Blog API!" });
+    });
+
+    app.listen(config.PORT, () => {
+      console.log(`Server is running on http://localhost:${config.PORT}`);
+    });
+  } catch (err) {
+    console.log("Failed to start the server:", err);
+
+    if (config.NODE_ENV === "production") {
+      process.exit(1);
+    }
+  }
+})();
