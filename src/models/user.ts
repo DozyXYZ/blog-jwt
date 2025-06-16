696954c6b851f4647b43089e4bf964f54f66dc31
Dozy <dotuan14747@gmail.com>
2025-06-16 12:50:57 +0300

    Set up User interface and schema. Set up controllers and models for authentication register

---
+++ src/models/user.ts
@@ -0,0 +1,99 @@
+import { Schema, model } from "mongoose";
+
+export interface IUser {
+  username: string;
+  email: string;
+  password: string;
+  role: "admin" | "user";
+  firstName?: string;
+  lastName?: string;
+  socialLinks?: {
+    website?: string;
+    twitter?: string;
+    instagram?: string;
+    facebook?: string;
+    youtube?: string;
+    linkedin?: string;
+    github?: string;
+  };
+}
+
+const userSchema = new Schema<IUser>(
+  {
+    username: {
+      type: String,
+      required: [true, "Username is required"],
+      maxLength: [20, "Username cannot exceed 20 characters"],
+      unique: [true, "Username must be unique"],
+    },
+
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      maxlength: [50, "Email cannot exceed 50 characters"],
+      unique: [true, "Email must be unique"],
+    },
+
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      select: false,
+    },
+
+    role: {
+      type: String,
+      required: [true, "Role is required"],
+      enum: {
+        values: ["admin", "user"],
+        message: "{VALUE} is not supported",
+      },
+      default: "user",
+    },
+
+    firstName: {
+      type: String,
+      maxlength: [20, "First name cannot exceed 20 characters"],
+    },
+
+    lastName: {
+      type: String,
+      maxlength: [20, "Last name cannot exceed 20 characters"],
+    },
+
+    socialLinks: {
+      website: {
+        type: String,
+        maxlength: [100, "Website address cannot exceed 100 characters"],
+      },
+      twitter: {
+        type: String,
+        maxlength: [50, "Twitter profile URL cannot exceed 50 characters"],
+      },
+      instagram: {
+        type: String,
+        maxlength: [50, "Instagram profile URL cannot exceed 50 characters"],
+      },
+      facebook: {
+        type: String,
+        maxlength: [50, "Facebook profile URL cannot exceed 50 characters"],
+      },
+      youtube: {
+        type: String,
+        maxlength: [50, "YouTube channel URL cannot exceed 50 characters"],
+      },
+      linkedin: {
+        type: String,
+        maxlength: [50, "LinkedIn profile URL cannot exceed 50 characters"],
+      },
+      github: {
+        type: String,
+        maxlength: [50, "GitHub profile URL cannot exceed 50 characters"],
+      },
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default model<IUser>("User", userSchema);
