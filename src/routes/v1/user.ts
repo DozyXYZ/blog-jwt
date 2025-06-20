e8788025d08b2a8209ac5ec969fc7ea67123fcf2
Dozy <dotuan14747@gmail.com>
2025-06-20 15:34:59 +0300

    Implemented get current user api

---
+++ src/routes/v1/user.ts
@@ -0,0 +1,17 @@
+import { Router } from "express";
+
+import authenticate from "@/middlewares/authenticate";
+import authorize from "@/middlewares/authorize";
+
+import getCurrentUser from "@/controllers/v1/user/get_current_user";
+
+const router = Router();
+
+router.get(
+  "/current",
+  authenticate,
+  authorize(["admin", "user"]),
+  getCurrentUser
+);
+
+export default router;
